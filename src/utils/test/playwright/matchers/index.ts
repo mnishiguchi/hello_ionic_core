2006d8ff2615d9d11cec1f4430c62e907ab1107e
Masatoshi N <nishiguchi.masa@gmail.com>
2022-07-29 22:20:23 -0400

    Remove playwright

--- src/utils/test/playwright/matchers/index.ts
+++
@@ -1,7 +0,0 @@
-import { toHaveReceivedEvent } from './toHaveReceivedEvent';
-import { toHaveReceivedEventDetail } from './toHaveReceivedEventDetail';
-
-export const matchers = {
-  toHaveReceivedEvent,
-  toHaveReceivedEventDetail,
-};
