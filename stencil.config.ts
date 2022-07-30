5c9a32ec33c09afbee0a391a986ba49cc918d786
Masatoshi N <nishiguchi.masa@gmail.com>
2022-07-29 20:11:38 -0400

    Simplify package.json

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,12 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-import { angularOutputTarget } from '@stencil/angular-output-target';
-import { vueOutputTarget } from '@stencil/vue-output-target';
-import { reactOutputTarget } from '@stencil/react-output-target';
 
 // @ts-ignore
 import { apiSpecGenerator } from './scripts/api-spec-generator';
 
 export const config: Config = {
   autoprefixCss: true,
-  namespace: 'Ionic',
+  namespace: 'ionic-custom',
   bundles: [
     { components: ['ion-action-sheet'] },
     { components: ['ion-alert'] },
@@ -63,94 +60,6 @@ export const config: Config = {
     })
   ],
   outputTargets: [
-    reactOutputTarget({
-      componentCorePackage: '@ionic/core',
-      includeImportCustomElements: true,
-      includePolyfills: false,
-      includeDefineCustomElements: false,
-      proxiesFile: '../packages/react/src/components/proxies.ts',
-      excludeComponents: [
-        // Routing
-        'ion-router',
-        'ion-route',
-        'ion-route-redirect',
-        'ion-router-link',
-        'ion-router-outlet',
-        'ion-back-button',
-        'ion-breadcrumb',
-        'ion-tab-button',
-        'ion-tabs',
-        'ion-tab-bar',
-        'ion-button',
-        'ion-card',
-        'ion-fab-button',
-        'ion-item',
-        'ion-item-option',
-
-        // Overlays
-        'ion-action-sheet',
-        'ion-alert',
-        'ion-loading',
-        'ion-modal',
-        'ion-picker',
-        'ion-popover',
-        'ion-toast',
-
-        'ion-app',
-        'ion-icon'
-      ]
-    }),
-    vueOutputTarget({
-      componentCorePackage: '@ionic/core',
-      includeImportCustomElements: true,
-      includePolyfills: false,
-      includeDefineCustomElements: false,
-      proxiesFile: '../packages/vue/src/proxies.ts',
-      excludeComponents: [
-        // Routing
-        'ion-router',
-        'ion-route',
-        'ion-route-redirect',
-        'ion-router-link',
-        'ion-router-outlet',
-        'ion-back-button',
-        'ion-tab-button',
-        'ion-tabs',
-        'ion-tab',
-        'ion-tab-bar',
-
-        // Overlays
-        'ion-action-sheet',
-        'ion-alert',
-        'ion-loading',
-        'ion-modal',
-        'ion-picker',
-        'ion-popover',
-        'ion-toast',
-
-        'ion-app',
-        'ion-icon'
-      ],
-      componentModels: [
-        {
-          elements: ['ion-checkbox', 'ion-toggle'],
-          targetAttr: 'checked',
-          event: 'v-ion-change',
-          externalEvent: 'ionChange'
-        },
-        {
-          elements: ['ion-datetime', 'ion-input', 'ion-radio-group', 'ion-radio', 'ion-range', 'ion-searchbar', 'ion-segment', 'ion-segment-button', 'ion-select', 'ion-textarea', 'ion-accordion-group'],
-          targetAttr: 'value',
-          event: 'v-ion-change',
-          externalEvent: 'ionChange'
-        }
-      ],
-    }),
-    {
-      type: 'docs-vscode',
-      file: 'dist/html.html-data.json',
-      sourceCodeBaseUrl: 'https://github.com/ionic-team/ionic/tree/main/core/',
-    },
     {
       type: 'dist',
       esmLoaderPath: '../loader'
@@ -175,77 +84,6 @@ export const config: Config = {
     apiSpecGenerator({
       file: 'api.txt'
     }) as any,
-    // {
-    //   type: 'stats',
-    //   file: 'stats.json'
-    // },
-    angularOutputTarget({
-      componentCorePackage: '@ionic/core',
-      directivesProxyFile: '../angular/src/directives/proxies.ts',
-      directivesArrayFile: '../angular/src/directives/proxies-list.txt',
-      excludeComponents: [
-        // overlays
-        'ion-action-sheet',
-        'ion-alert',
-        'ion-loading',
-        'ion-modal',
-        'ion-picker',
-        'ion-popover',
-        'ion-toast',
-        'ion-toast',
-
-        // navigation
-        'ion-router',
-        'ion-route',
-        'ion-route-redirect',
-        'ion-router-link',
-        'ion-router-outlet',
-
-        // tabs
-        'ion-tabs',
-        'ion-tab',
-
-        // auxiliar
-        'ion-picker-column',
-        'ion-virtual-scroll'
-      ],
-      /**
-       * TODO: Abstract custom Ionic value accessor functionality
-       * to be configurable with Stencil generated value accessors.
-       */
-      // valueAccessorConfigs: [
-      //   {
-      //     elementSelectors: ['ion-input:not([type=number])', 'ion-textarea', 'ion-searchbar'],
-      //     event: 'ionChange',
-      //     targetAttr: 'value',
-      //     type: 'text',
-      //   },
-      //   {
-      //     elementSelectors: ['ion-input[type=number]'],
-      //     event: 'ionChange',
-      //     targetAttr: 'value',
-      //     type: 'number',
-      //   },
-      //   {
-      //     elementSelectors: ['ion-checkbox', 'ion-toggle'],
-      //     event: 'ionChange',
-      //     targetAttr: 'checked',
-      //     type: 'boolean',
-      //   },
-      //   {
-      //     elementSelectors: ['ion-range', 'ion-select', 'ion-radio-group', 'ion-segment', 'ion-datetime'],
-      //     event: 'ionChange',
-      //     targetAttr: 'value',
-      //     type: 'select',
-      //   },
-      //   {
-      //     elementSelectors: ['ion-radio'],
-      //     event: 'ionSelect',
-      //     targetAttr: 'checked',
-      //     type: 'radio',
-      //   },
-      // ]
-    }),
   ],
   buildEs5: 'prod',
   extras: {
@@ -262,33 +100,8 @@ export const config: Config = {
       "@utils/test": ["<rootDir>/src/utils/test/utils"],
       "@utils/logging": ["<rootDir>/src/utils/logging"],
 
-    },
-    emulate: [
-      {
-        userAgent: 'iPhone',
-        viewport: {
-          width: 400,
-          height: 800,
-          deviceScaleFactor: 2,
-          isMobile: true,
-          hasTouch: true,
-          isLandscape: false
-        }
-      },
-      {
-        userAgent: 'Android',
-        viewport: {
-          width: 400,
-          height: 800,
-          deviceScaleFactor: 2,
-          isMobile: true,
-          hasTouch: true,
-          isLandscape: false
-        }
-      }
-    ]
+    }
   },
-  preamble: '(C) Ionic http://ionicframework.com - MIT License',
   globalScript: 'src/global/ionic-global.ts',
   enableCache: true,
 };
